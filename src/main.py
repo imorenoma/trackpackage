3a85f6eb6f3c8ed45734e2b6c1205850d9c89e73
ivan <iv.morenoma@gmail.com>
2024-01-25 10:43:12 +0100

    crete two menu options to create registry and check database

--- src/main.py
+++ src/main.py
@@ -1,40 +1,53 @@
-
 import querys
 
 def main():
-    print('\n')
-    print("------------------------------")    
-    print("Chose the proper option")
-    print("------------------------------",'\n')
-
-    print("1. Insert package Track number")
-    print("2. Move date delivery")
-    print("3. Cancel delivery")
-    print("4. List of track numbers")
-    print("5. Exit app")
 
     print('\n')
-
-    options = {
-        '1': querys.insert_data(),
-        # '2': app.modify_data(),
-        # '3': app.delete_data(),
-        # '4': app.send_query(),
-        # '5': app.send_query(),
-        # '6': app.exit()
-    }
-
+    print("******************************")
+    print("Welcome To TrackPackage!")
+    print("******************************",'\n')
     try:
-        input_option = input("Select yor option: ")
-        
-        if input_option == '1':
-            print(options["1"],)
+        while True:
 
+            print("------------------------------")    
+            print("Select a option")
+            print("------------------------------",'\n')
+
+            print("1. Create Registry")
+            print("2. Read database")
+            print("3. Update delivery")
+            print("4. Delete database")
+            print("5. Exit app")
+
+            print('\n')        
+
+            
+            option = int(input("Select yor option: "))
+                
+            if option == 1:
+                request = querys.create_Registry()
+                if request == 2:
+                    break
+                if request == 1:
+                    continue
+            elif option == 2:
+                request = querys.check_Database()
+                if request == 2:
+                    break
+                if request == 1:
+                    continue
+                # elif option == 3:
+                # elif option == 4:
+                # elif option == 5:
+            else:
+                print("[Error]: Please insert a valid argument")
+                print("[Usage]: insert numner between 1 and 5")
+                continue
     except KeyboardInterrupt:
         print('\n')
         print("Clossing App ...")
-        print("Bye")
         print('\n')
 
 if __name__ == "__main__":
     main()
+
